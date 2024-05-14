0d3ef8224dba07b3681e383e59259be0bd6bac6b
Joel Joby <joeljobyp@gmail.com>
2024-05-14 19:56:36 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,6 +1,12 @@
 from flask import Flask
-
+from flask_cors import CORS, cross_origin
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+@app.route("/app/test",methods=["GET"])
+def test_reply():
+    return {"messgage":"Suscess"}
 
 @app.route('/')
 def home():
