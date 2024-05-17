b76157274de2f6397750b7ec6e0a2e3ac6175b8e
Joel Joby <joeljobyp@gmail.com>
2024-05-17 17:54:01 +0530

    updated

--- api/index.py
+++ api/index.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_cors import CORS, cross_origin
+from youtubesearchpython.__future__ import VideosSearch
 app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
@@ -15,3 +16,16 @@ def home():
 @app.route('/about')
 def about():
     return 'About'
+
+@app.route('/search/<query>')
+async def Search(query):
+    videosSearch = VideosSearch(query, limit = 2)
+    videosResult =  await videosSearch.next()
+    print(videosResult["result"])
+    title = videosResult["result"][0]["accessibility"]["title"]
+    preview = "https://www.youtube.com/watch?v=7tNtU5XFwrU"
+    #print(video_search_results["result"])
+    return {"title":title,"preview":preview}
+
+if __name__ == "__main__":
+    app.run(port=8000,debug=True)   
